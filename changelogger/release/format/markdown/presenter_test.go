b2f7837e43fecdcb4c9ee2b2056093b1d1bbee16
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-20 02:07:44 +0600

    test

--- changelogger/release/format/markdown/presenter_test.go
+++ changelogger/release/format/markdown/presenter_test.go
@@ -52,8 +52,8 @@ func TestMarkdownPresenter_Present(t *testing.T) {
 						Version: "v0.19.1",
 						Date:    time.Date(2021, time.September, 16, 19, 34, 0, 0, time.UTC),
 					},
-					VCSReferenceURL: "https://github.com/khulnasoft-labs/syft/tree/v0.19.1",
-					VCSChangesURL:   "https://github.com/khulnasoft-labs/syft/compare/v0.19.0...v0.19.1",
+					VCSReferenceURL: "https://github.com/anchore/syft/tree/v0.19.1",
+					VCSChangesURL:   "https://github.com/anchore/syft/compare/v0.19.0...v0.19.1",
 					Changes: []change.Change{
 						{
 							ChangeTypes: []change.Type{change.NewType("bug", change.SemVerPatch)},
@@ -61,7 +61,7 @@ func TestMarkdownPresenter_Present(t *testing.T) {
 							References: []change.Reference{
 								{
 									Text: "456",
-									URL:  "https://github.com/khulnasoft-labs/syft/pull/456",
+									URL:  "https://github.com/anchore/syft/pull/456",
 								},
 							},
 						},
